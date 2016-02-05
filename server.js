0146956db05e0b67ef919e048fb1139837da3d9b
case <case@casesandberg.com>
2016-02-05 14:26:02 -0600

    Add Atom

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 'use strict';
 
-const httpServer = require('http-server');
+require('babel-register');
+const proc = require('child_process');
 const socketEmitter = require('chokidar-socket-emitter');
 
 const port = process.argv[2] || 8090;
 
-const server = httpServer.createServer({
-  cache: -1,
-  root: './',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-  },
-});
+socketEmitter({ app: port });
 
-socketEmitter({ app: server.server });
-
-server.listen(port);
-console.log(`http://localhost:${ port }`);
+proc.exec('electron .', (error) => { console.log(error); });
